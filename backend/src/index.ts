80dc15c2add1f86983e60cc2e39c5c8981d382c7
Rawan Sameh <Rawansamehhosny1@gmail.com>
2026-02-19 22:04:14 +0200

    تعديل

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import { cors } from "hono/cors";
 const app = new Hono();
 import 'dotenv/config'; 
 app.use("*", cors());
-import { Model } from './models/model.ts'; 
+import { Model } from './models/model.js'; 
 import mongoose from 'mongoose';
 app.use('/uploads/*', serveStatic({ root: './public' }));
 import { createClient } from '@supabase/supabase-js'
